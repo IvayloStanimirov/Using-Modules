53ccd64476c71b59a9d8706e6573a4edb8549e82
Ivaylo <kolevivaylo27@gmail.com>
2021-09-26 00:21:34 +0300

    installing ramadajs and adding classes to the articles in the index.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,8 @@
 import "../scss/app.scss";
 
+
+const R = require('ramda');
+// Importing ramada.js
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
@@ -8,5 +11,18 @@ window.addEventListener("DOMContentLoaded", () => {
     { age: 23, class: "is-warning" },
     { job: "programmer", class: "is-danger" },
   ];
+  var getAges = R.pluck('class');
+  // Getting the classes from the ArrayToPluck
+  let arrayOfTheClasses =  getAges(arrayToPluck);
+  // Storing the classes in a array
+  let counter = 0;
   const articles = document.querySelectorAll("article");
+
+  articles.forEach((articele) => {
+      
+      articele.classList.add(arrayOfTheClasses[counter]);
+      counter++;
+  })
+  // Adding the classes to the articles
+  
 });
